55642911fc71d54f9977bb239670860e709a5f98
giosue <giosue_liuzzo@hotmail.be>
2020-07-04 19:41:46 +0200

    Commit

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 //Utilsation du framework express via app
 const app = express();
+//Import du package pour gerer la database mongodb
+const mongoose = require('mongoose');
+//Import du model de schema
+const Thing = require('./models/Thing');
 
-
+mongoose.connect('mongodb+srv://giosue:elisa2812@formationopenapinodejse.ubzce.mongodb.net/test?retryWrites=true&w=majority',
+    {useNewUrlParser: true,
+    useUnifiedTopology: true}).then(()=> console.log('Connexion a MongoDB réussie !')).catch(()=>console.log('Connexion a MongoDB échouée'));
 // //Reponse qui indique que la requete est reçue puis passe a l'execution
 // app.use((req,res,next)=>{
 //     console.log('Requete reçue !');
@@ -34,35 +40,40 @@ const app = express();
     });
 
 app.use(bodyParser.json());
-
+//Route pour enregistrer un nouveau opbjet
 app.post('/api/stuff',(req,res,next)=>{
-    console.log(req.body);
-    res.status(201).json({
-        message:'Objet crée !'
+    delete req.body._id;
+    const thing = new Thing({
+        ...req.body
     });
+    thing.save()
+        .then(() => res.status(201).json({message: 'Objet enregisté !'}))
+        .catch(error => res.status(400).json({error}));
+});
+//Route pour recuperer un objet specifique
+app.get('/api/stuff/:id',(req,res,next)=> {
+    Thing.findOne({_id: req.params.id})
+        .then(thing => res.status(200).json(thing))
+        .catch(error => res.status(404).json({error}));
+});
+//Route pour modifier un objet
+app.put('/api/stuff/:id',(req,res,next)=> {
+    Thing.updateOne({_id: req.params.id},{...req.body,_id: req.params.id})
+        .then(() => res.status(200).json({message: 'Objet modifié'}))
+        .catch(error => res.status(400).json({error}));
+});
+
+//Route pour delete un objet
+app.delete('/api/stuff/:id',(req,res,next)=> {
+    Thing.deleteOne({_id: req.params.id})
+        .then(() => res.status(200).json({message: 'Objet supprimé !'}))
+        .catch(error => res.status(400).json({error}));
 });
 //Premiere route pour notre api
 app.use('/api/stuff',(req,res,next)=>{
-    const stuff = [
-        {
-            _id: 'oeihzeoi',
-            title:'Mon premier objet',
-            description: 'les info de mon premier objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 4900,
-            userId: 'qsomihvqios',
-        },
-        {
-            _id: 'oeihfzeomoihi',
-            title:'Mon deuxieme objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900,
-            userId: 'qsomihvqios',
-        }
-    ];
-    //Reponse renvoyer avec les fata
-    res.status(200).json(stuff);
+    Thing.find()
+        .then(things => res.status(200).json(things))
+        .catch(error => res.status(400).json(error));
 });
 //Export du module app
 module.exports = app;
